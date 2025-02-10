a77e263803ed2c096739cbcfd55261d3b9511593
Dimas Ahmad Rizal <mail.dimasrizal@gmail.com>
2025-02-10 21:47:56 +0700

    fix position register

--- resources/views/livewire/auth/register-choice.blade.php
+++ resources/views/livewire/auth/register-choice.blade.php
@@ -10,20 +10,6 @@
 
     <div class="mt-12 max-w-6xl mx-auto">
         <div class="grid grid-cols-1 lg:grid-cols-2 gap-8">
-            <!-- Advertiser -->
-            <div class="bg-white rounded-lg p-8 text-center">
-                <div class="flex justify-center items-center mb-6">
-                    <div class="bg-green-500 text-white w-16 h-16 flex items-center justify-center rounded-full">
-                        <i class="fas fa-bullhorn text-3xl"></i>
-                    </div>
-                </div>
-                <h2 class="text-2xl font-semibold mb-4">Sebagai Pengiklan</h2>
-                <p class="text-gray-600 mb-6">Saya ingin beriklan, saya ingin memberdayakan pasukan iklan (adsman) Spartav untuk mempromosikan brand / produk saya.</p>
-                <a href="{{ route('pengiklan.register') }}"
-                    class="inline-block bg-green-500 text-white px-6 py-3 rounded-lg hover:bg-green-600 text-lg font-medium">Daftar
-                    Sekarang</a>
-            </div>
-
             <!-- Komentator -->
             <div class="bg-white rounded-lg p-8 text-center">
                 <div class="flex justify-center items-center mb-6">
@@ -35,6 +21,20 @@
                 <p class="text-gray-600 mb-6">Saya ingin mendapatkan penghasilan tambahan dengan melakukan kegiatan promosi pemasaran, penjualan, dan iklan di akun medsos saya.</p>
                 <a href="{{ route('pasukan.register') }}"
                     class="inline-block bg-yellow-500 text-white px-6 py-3 rounded-lg hover:bg-yellow-600 text-lg font-medium">Daftar
+                    Sekarang</a>
+            </div>
+
+            <!-- Advertiser -->
+            <div class="bg-white rounded-lg p-8 text-center">
+                <div class="flex justify-center items-center mb-6">
+                    <div class="bg-green-500 text-white w-16 h-16 flex items-center justify-center rounded-full">
+                        <i class="fas fa-bullhorn text-3xl"></i>
+                    </div>
+                </div>
+                <h2 class="text-2xl font-semibold mb-4">Sebagai Pengiklan</h2>
+                <p class="text-gray-600 mb-6">Saya ingin beriklan, saya ingin memberdayakan pasukan iklan (adsman) Spartav untuk mempromosikan brand / produk saya.</p>
+                <a href="{{ route('pengiklan.register') }}"
+                    class="inline-block bg-green-500 text-white px-6 py-3 rounded-lg hover:bg-green-600 text-lg font-medium">Daftar
                     Sekarang</a>
             </div>
         </div>
