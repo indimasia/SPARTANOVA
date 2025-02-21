609fd2246acb036b073ad3dcb9b63795edb33de9
Dimas Ahmad Rizal <mail.dimasrizal@gmail.com>
2025-02-21 23:51:54 +0700

    fix merge

--- app/Filament/Resources/UserResource/Pages/CreateUser.php
+++ app/Filament/Resources/UserResource/Pages/CreateUser.php
@@ -2,11 +2,45 @@
 
 namespace App\Filament\Resources\UserResource\Pages;
 
-use App\Filament\Resources\UserResource;
 use Filament\Actions;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Database\Eloquent\Model;
+use App\Filament\Resources\UserResource;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateUser extends CreateRecord
 {
     protected static string $resource = UserResource::class;
+
+    protected function handleRecordCreation(array $data): Model
+    {
+        $user = static::getModel()::create($data);
+
+        if (isset($data)) {
+            $socialMedias = [
+                'Instagram' => $data['instagram'] ?? null,
+                'TikTok' => $data['tiktok'] ?? null,
+                'Youtube' => $data['youtube'] ?? null,
+                'Facebook' => $data['facebook'] ?? null,
+                'Twitter' => $data['twitter'] ?? null,
+                'Google' => $data['google'] ?? null,
+                'WhatsApp' => $data['whatsapp'] ?? null,
+            ];
+
+            foreach ($socialMedias as $sosisal_media => $account) {
+                if ($account) {
+                    \App\Models\SosialMediaAccount::create([
+                        'user_id' => $user->id,
+                        'sosial_media' => $sosisal_media,
+                        'account' => $account,
+
+
+                    ]);
+                }
+            }
+        }
+
+        return $user;
+        }
+
 }
--- app/Filament/Resources/UserResource/Pages/EditUser.php
+++ app/Filament/Resources/UserResource/Pages/EditUser.php
@@ -2,13 +2,62 @@
 
 namespace App\Filament\Resources\UserResource\Pages;
 
-use App\Filament\Resources\UserResource;
 use Filament\Actions;
+use App\Models\SosialMediaAccount;
+use App\Filament\Resources\UserResource;
 use Filament\Resources\Pages\EditRecord;
 
 class EditUser extends EditRecord
 {
     protected static string $resource = UserResource::class;
+
+    protected function mutateFormDataBeforeFill(array $data): array
+    {
+        $socialMedia = $this->record->sosialMediaAccounts->pluck('account', 'sosial_media')->toArray();
+
+        $data['instagram'] = $socialMedia['Instagram'] ?? null;
+        $data['tiktok'] = $socialMedia['TikTok'] ?? null;
+        $data['youtube'] = $socialMedia['Youtube'] ?? null;
+        $data['facebook'] = $socialMedia['Facebook'] ?? null;
+        $data['twitter'] = $socialMedia['Twitter'] ?? null;
+        $data['google'] = $socialMedia['Google'] ?? null;
+        $data['whatsapp'] = $socialMedia['WhatsApp'] ?? null;
+
+        return $data;
+    }
+
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        $user = $this->record;
+
+        $socialMedias = [
+            'Instagram' => $data['instagram'] ?? null,
+            'TikTok' => $data['tiktok'] ?? null,
+            'Youtube' => $data['youtube'] ?? null,
+            'Facebook' => $data['facebook'] ?? null,
+            'Twitter' => $data['twitter'] ?? null,
+            'Google' => $data['google'] ?? null,
+            'WhatsApp' => $data['whatsapp'] ?? null,
+        ];
+
+        foreach ($socialMedias as $platform => $account) {
+            if ($account !== null) {
+                SosialMediaAccount::updateOrCreate(
+                    ['user_id' => $user->id, 'sosial_media' => $platform],
+                    ['account' => $account]
+                );
+            } else {
+                SosialMediaAccount::updateOrCreate(
+                    ['user_id' => $user->id, 'sosial_media' => $platform],
+                    ['account' => 'Tidak punya akun']
+                );
+            }
+
+        }
+
+        return $data;
+    }
+
 
     protected function getHeaderActions(): array
     {
