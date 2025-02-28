fbb52097d9bef7a074a487d917e1dc721d5039f5
Dimas Ahmad Rizal <mail.dimasrizal@gmail.com>
2025-03-01 00:02:56 +0700

    fix structur dashboard-pasukan

--- resources/views/livewire/pasukan/dashboard.blade.php
+++ resources/views/livewire/pasukan/dashboard.blade.php
@@ -8,6 +8,105 @@
             </h2>
             {{-- <button onclick="subscribeUser()">Aktifkan Notifikasi</button> --}}
 
+            <div class="bg-white border border-gray-100 rounded-lg mt-14">
+                <div class="p-4 border-b border-gray-100">
+                    <h3 class="text-lg font-semibold text-gray-800">Pengumuman Terbaru</h3>
+                </div>
+                <div class="p-4">
+
+                    @if (!empty($annoucements) )     
+                    
+                            <div x-data="{            
+                                slides: [                
+                                  @foreach ($annoucements as $annoucement)
+                                {
+                                    imgSrc: '{{ $annoucement['image'] ? URL::route('storage.fetch', ['filename' => $annoucement['image']]) : null, }}',
+                                    imgAlt: 'Vibrant abstract painting with swirling blue and light pink hues on a canvas.',  
+                                    title: '{{ $annoucement['title'] }}',
+                                    description: '{{ $annoucement['content'] }}',    
+                                    ctaUrl: 'https://example.com',
+                                    ctaText: 'Become a Developer',          
+                                    },                
+                                    @endforeach
+                                    ],             
+                                currentSlideIndex: 1,
+                                previous() {                
+                                    if (this.currentSlideIndex > 1) {                    
+                                        this.currentSlideIndex = this.currentSlideIndex - 1                
+                                    } else {   
+                                        // If it's the first slide, go to the last slide           
+                                        this.currentSlideIndex = this.slides.length                
+                                    }            
+                                },            
+                                next() {                
+                                    if (this.currentSlideIndex < this.slides.length) {                    
+                                        this.currentSlideIndex = this.currentSlideIndex + 1                
+                                    } else {                 
+                                        // If it's the last slide, go to the first slide    
+                                        this.currentSlideIndex = 1                
+                                    }            
+                                },        
+                            }" class="relative w-full overflow-hidden">
+                            
+                                <!-- previous button -->
+                                <button type="button" class="absolute left-5 top-1/2 z-20 flex rounded-full -translate-y-1/2 items-center justify-center bg-surface/40 p-2 text-on-surface transition hover:bg-surface/60 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary active:outline-offset-0 dark:bg-surface-dark/40 dark:text-on-surface-dark dark:hover:bg-surface-dark/60 dark:focus-visible:outline-primary-dark" aria-label="previous slide" x-on:click="previous()">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" fill="none" stroke-width="3" class="size-5 md:size-6 pr-0.5" aria-hidden="true">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+                                    </svg>
+                                </button>
+                            
+                                <!-- next button -->
+                                <button type="button" class="absolute right-5 top-1/2 z-20 flex rounded-full -translate-y-1/2 items-center justify-center bg-surface/40 p-2 text-on-surface transition hover:bg-surface/60 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary active:outline-offset-0 dark:bg-surface-dark/40 dark:text-on-surface-dark dark:hover:bg-surface-dark/60 dark:focus-visible:outline-primary-dark" aria-label="next slide" x-on:click="next()">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" fill="none" stroke-width="3" class="size-5 md:size-6 pl-0.5" aria-hidden="true">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                                    </svg>
+                                </button>
+                               
+                                <!-- slides -->
+                                <div class="relative min-h-[50svh] w-full">
+                                    <template x-for="(slide, index) in slides">
+                                        <div x-cloak x-show="currentSlideIndex == index + 1" class="absolute inset-0" x-transition.opacity.duration.1000ms>
+
+                                            <article class="group grid rounded-radius max-full grid-cols-1 md:grid-cols-8 overflow-hidden border border-outline bg-surface-alt text-on-surface dark:border-outline-dark dark:bg-surface-dark-alt dark:text-on-surface-dark">
+                                                <!-- image -->
+                                                <div class="col-span-3 overflow-hidden">
+                                                    <img src='' class="md:h-[90%] w-[90%] object-cover transition duration-700 ease-out group-hover:scale-105" alt="a men wearing VR goggles" x-bind:src="slide.imgSrc" x-bind:alt="slide.imgAlt" />
+                                                </div>
+                                                <!-- body -->
+                                                <div class="flex flex-col  p-6 col-span-5">
+                                                    {{-- <small class="mb-4 font-medium">Artificial Intelligence</small> --}}
+                                                    <h3 class="text-balance text-xl font-bold text-on-surface-strong lg:text-2xl dark:text-on-surface-dark-strong"   x-text="slide.title" x-bind:aria-describedby="'slide' + (index + 1) + 'Description'" ></h3>
+                                                    <p id="articleDescription" class="my-4 max-w-lg text-pretty text-sm" x-text="slide.description" x-bind:id="'slide' + (index + 1) + 'Description'">
+                                                    </p>
+                                                </div>
+                                            </article>
+                                        </div>
+                                    </template>
+                                </div>
+                                
+                                <!-- indicators -->
+                                <div class="absolute rounded-radius bottom-3 md:bottom-5 left-1/2 z-20 flex -translate-x-1/2 gap-4 md:gap-3 px-1.5 py-1 md:px-2" role="group" aria-label="slides" >
+                                    <template x-for="(slide, index) in slides">
+                                        <button class="size-2 rounded-full transition" x-on:click="currentSlideIndex = index + 1" x-bind:class="[currentSlideIndex === index + 1 ? 'bg-on-surface-dark' : 'bg-on-surface-dark/50']" x-bind:aria-label="'slide ' + (index + 1)"></button>
+                                    </template>
+                                </div>
+                            </div>
+
+                            @else
+
+                            <div class="text-center py-6">
+                                <div class="inline-flex items-center justify-center w-12 h-12 rounded-full bg-gray-100 mb-3">
+                                    <i class="fas fa-clock text-xl text-gray-400"></i>
+                                </div>
+                                <h3 class="text-sm font-medium text-gray-900">Belum Ada Pengumuman</h3>
+                                <p class="text-sm text-gray-500 mt-1">PEngumuman akan muncul di sini</p>
+                            </div>
+                
+                    @endif
+                        </div>
+                </div>
+            </div>
+
 
             <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mb-6">
                 <div class="bg-white border border-gray-100 rounded-lg p-4 hover:shadow-md transition-shadow duration-200">
@@ -130,104 +229,7 @@
                 </div>
             </div>
 
-            <div class="bg-white border border-gray-100 rounded-lg mt-14">
-                <div class="p-4 border-b border-gray-100">
-                    <h3 class="text-lg font-semibold text-gray-800">Pengumuman Terbaru</h3>
-                </div>
-                <div class="p-4">
-
-                    @if (!empty($annoucements) )     
-                    
-                            <div x-data="{            
-                                slides: [                
-                                  @foreach ($annoucements as $annoucement)
-                                {
-                                    imgSrc: '{{ $annoucement['image'] ? URL::route('storage.fetch', ['filename' => $annoucement['image']]) : null, }}',
-                                    imgAlt: 'Vibrant abstract painting with swirling blue and light pink hues on a canvas.',  
-                                    title: '{{ $annoucement['title'] }}',
-                                    description: '{{ $annoucement['content'] }}',    
-                                    ctaUrl: 'https://example.com',
-                                    ctaText: 'Become a Developer',          
-                                    },                
-                                    @endforeach
-                                    ],             
-                                currentSlideIndex: 1,
-                                previous() {                
-                                    if (this.currentSlideIndex > 1) {                    
-                                        this.currentSlideIndex = this.currentSlideIndex - 1                
-                                    } else {   
-                                        // If it's the first slide, go to the last slide           
-                                        this.currentSlideIndex = this.slides.length                
-                                    }            
-                                },            
-                                next() {                
-                                    if (this.currentSlideIndex < this.slides.length) {                    
-                                        this.currentSlideIndex = this.currentSlideIndex + 1                
-                                    } else {                 
-                                        // If it's the last slide, go to the first slide    
-                                        this.currentSlideIndex = 1                
-                                    }            
-                                },        
-                            }" class="relative w-full overflow-hidden">
-                            
-                                <!-- previous button -->
-                                <button type="button" class="absolute left-5 top-1/2 z-20 flex rounded-full -translate-y-1/2 items-center justify-center bg-surface/40 p-2 text-on-surface transition hover:bg-surface/60 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary active:outline-offset-0 dark:bg-surface-dark/40 dark:text-on-surface-dark dark:hover:bg-surface-dark/60 dark:focus-visible:outline-primary-dark" aria-label="previous slide" x-on:click="previous()">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" fill="none" stroke-width="3" class="size-5 md:size-6 pr-0.5" aria-hidden="true">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
-                                    </svg>
-                                </button>
-                            
-                                <!-- next button -->
-                                <button type="button" class="absolute right-5 top-1/2 z-20 flex rounded-full -translate-y-1/2 items-center justify-center bg-surface/40 p-2 text-on-surface transition hover:bg-surface/60 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary active:outline-offset-0 dark:bg-surface-dark/40 dark:text-on-surface-dark dark:hover:bg-surface-dark/60 dark:focus-visible:outline-primary-dark" aria-label="next slide" x-on:click="next()">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" fill="none" stroke-width="3" class="size-5 md:size-6 pl-0.5" aria-hidden="true">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-                                    </svg>
-                                </button>
-                               
-                                <!-- slides -->
-                                <div class="relative min-h-[50svh] w-full">
-                                    <template x-for="(slide, index) in slides">
-                                        <div x-cloak x-show="currentSlideIndex == index + 1" class="absolute inset-0" x-transition.opacity.duration.1000ms>
-
-                                            <article class="group grid rounded-radius max-full grid-cols-1 md:grid-cols-8 overflow-hidden border border-outline bg-surface-alt text-on-surface dark:border-outline-dark dark:bg-surface-dark-alt dark:text-on-surface-dark">
-                                                <!-- image -->
-                                                <div class="col-span-3 overflow-hidden">
-                                                    <img src='' class="md:h-[90%] w-[90%] object-cover transition duration-700 ease-out group-hover:scale-105" alt="a men wearing VR goggles" x-bind:src="slide.imgSrc" x-bind:alt="slide.imgAlt" />
-                                                </div>
-                                                <!-- body -->
-                                                <div class="flex flex-col  p-6 col-span-5">
-                                                    {{-- <small class="mb-4 font-medium">Artificial Intelligence</small> --}}
-                                                    <h3 class="text-balance text-xl font-bold text-on-surface-strong lg:text-2xl dark:text-on-surface-dark-strong"   x-text="slide.title" x-bind:aria-describedby="'slide' + (index + 1) + 'Description'" ></h3>
-                                                    <p id="articleDescription" class="my-4 max-w-lg text-pretty text-sm" x-text="slide.description" x-bind:id="'slide' + (index + 1) + 'Description'">
-                                                    </p>
-                                                </div>
-                                            </article>
-                                        </div>
-                                    </template>
-                                </div>
-                                
-                                <!-- indicators -->
-                                <div class="absolute rounded-radius bottom-3 md:bottom-5 left-1/2 z-20 flex -translate-x-1/2 gap-4 md:gap-3 px-1.5 py-1 md:px-2" role="group" aria-label="slides" >
-                                    <template x-for="(slide, index) in slides">
-                                        <button class="size-2 rounded-full transition" x-on:click="currentSlideIndex = index + 1" x-bind:class="[currentSlideIndex === index + 1 ? 'bg-on-surface-dark' : 'bg-on-surface-dark/50']" x-bind:aria-label="'slide ' + (index + 1)"></button>
-                                    </template>
-                                </div>
-                            </div>
-
-                            @else
-
-                            <div class="text-center py-6">
-                                <div class="inline-flex items-center justify-center w-12 h-12 rounded-full bg-gray-100 mb-3">
-                                    <i class="fas fa-clock text-xl text-gray-400"></i>
-                                </div>
-                                <h3 class="text-sm font-medium text-gray-900">Belum Ada Pengumuman</h3>
-                                <p class="text-sm text-gray-500 mt-1">PEngumuman akan muncul di sini</p>
-                            </div>
-                
-                    @endif
-                        </div>
-                </div>
-        </div>
+            
     </div>
     <div id="location-modal" class="fixed inset-0 z-50 flex items-center justify-center bg-gray-500 bg-opacity-50 hidden">
         <div class="bg-white p-6 rounded-lg shadow-lg w-full max-w-md mx-4 sm:mx-0">
