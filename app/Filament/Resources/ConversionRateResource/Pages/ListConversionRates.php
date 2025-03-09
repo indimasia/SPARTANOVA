a6835f7de961b179bcf829213027d42516643656
Dimas Ahmad Rizal <mail.dimasrizal@gmail.com>
2025-03-09 16:28:31 +0700

    fix create button

--- app/Filament/Resources/ConversionRateResource/Pages/ListConversionRates.php
+++ app/Filament/Resources/ConversionRateResource/Pages/ListConversionRates.php
@@ -13,7 +13,7 @@ class ListConversionRates extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            // Actions\CreateAction::make(),
+            Actions\CreateAction::make(),
         ];
     }
 }
